00d20b812315e8e35dd09dde66f2985e1c68f804
Alienware <alienware@pop-os.localdomain>
2021-04-10 15:04:35 +0530

    fix sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

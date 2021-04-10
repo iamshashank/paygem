611248c10dcb4be7579306d34e9ff84191230a80
Alienware <alienware@pop-os.localdomain>
2021-04-10 15:52:46 +0530

    fix smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,7 +122,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'https://obscure-caverns-44111.herokuapp.com',
+    :domain         => 'heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp

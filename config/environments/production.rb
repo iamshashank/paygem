48aa4e19de0005b25c8f0dac7ecac33113fa878b
Alienware <alienware@pop-os.localdomain>
2021-04-10 15:00:23 +0530

    initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,3 +118,12 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'https://obscure-caverns-44111.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  mount Sidekiq::Web => "/sidekiq"
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
